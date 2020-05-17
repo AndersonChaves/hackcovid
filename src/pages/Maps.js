a3ffd2d99170c2dd126eb4621855c1089efc9031
Gustavo Chermout <gustavo_chermout@hotmail.com>
2020-05-17 10:18:36 -0300

    Update maps page

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -1,16 +1,60 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Maps() {
-  return (
-    <View style={styles.container}>
+  const [currentRegion, setCurrentRegion] = useState(null);
 
-    </View>
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted){
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04, 
+          longitudeDelta: 0.04,
+        });
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  function handleRegionChanged(region) {
+    setCurrentRegion(region);
+  }
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <MapView 
+      onRegionChangeComplete={handleRegionChanged} 
+      initialRegion={currentRegion} 
+      style={styles.map}
+    >
+      <Marker 
+        coordinate={{ 
+          longitude: currentRegion.longitude,
+          latitude: currentRegion.latitude,         
+        }}
+      />
+    </MapView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  map: {
     flex: 1,
   },
 });
